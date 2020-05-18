288e8c846b7465c75fa27d6d6cfa3b17f9d69499
Chao Jiang <775336037@qq.com>
2020-05-18 18:40:32 +0800

    Database migrations with Flask-Migrate

--- hello.py
+++ hello.py
@@ -8,15 +8,17 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:RVhT2c!v@localhost:3306/new_demo1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:RVhT2c!v@localhost:3306/new_demo2'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Role(db.Model):
@@ -34,6 +36,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    address = db.Column(db.String(64), unique=True, index=True)
+    phone = db.Column(db.String(64), unique=True, index=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
