c13c63a80beac2065128088643a27be7e36632ee
Chao Jiang <775336037@qq.com>
2020-05-26 15:02:11 +0800

    Password hashing with Werkzeug

--- config.py
+++ config.py
@@ -31,16 +31,16 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:RVhT2c!v@localhost:3306/devmysql'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:RVhT2c!v@localhost:3306/devmysql?charset=utf8mb4'
 
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:RVhT2c!v@localhost:3306/testmysql'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:RVhT2c!v@localhost:3306/testmysql?charset=utf8mb4'
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:RVhT2c!v@localhost:3306/promysql'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:RVhT2c!v@localhost:3306/promysql?charset=utf8mb4'
 
 
 config = {
