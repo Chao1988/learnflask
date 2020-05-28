31927014b2305aba9a604bf1e5e30010cb7b8daa
Chao Jiang <775336037@qq.com>
2020-05-28 21:54:53 +0800

    Login and logout with Flask-Login

---
+++ app/auth/forms.py
@@ -0,0 +1,18 @@
+"""
+-------------------------------------------------
+   Project Name:  LearnFlask
+   File Name:     forms
+   Author:        cjiang
+   Date:          2020/5/28 5:16 PM
+-------------------------------------------------
+"""
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
--- app/auth/views.py
+++ app/auth/views.py
@@ -6,10 +6,32 @@
    Date:          2020/5/26 6:37 PM
 -------------------------------------------------
 """
-from flask import render_template
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user
 from . import auth
+from ..models import User
+from .forms import LoginForm
+from flask_login import logout_user, login_required
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            next = request.args.get('next')
+            if next is None or not next.startswitch('/'):
+                next = url_for('main.index')
+            return redirect(next)
+        flash('Invalid username and password.')
+    return render_template('auth/login.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
